cc535664bbfe97a32d6be12c419a816104f40a2e
Herrera Rocha <fabioesteban97@hotmail.com>
2025-02-14 13:41:09 +0100

    Added documentation to ChemBERTa encoding

--- code_and_notebooks/featurization.py
+++ code_and_notebooks/featurization.py
@@ -5,6 +5,26 @@ from transformers import AutoModelForMaskedLM, Autotokenizer
 class LLMsEncoding:
 
     def featurize_ChemBERTa(smiles_list, padding=True):
+        """
+    Featurizes a list of SMILES strings using the ChemBERTa-77M-MTR model.
+
+    Parameters:
+    -----------
+    smiles_list : list of str
+        A list of SMILES strings representing molecular structures.
+    padding : bool, optional (default=True)
+        Whether to pad the tokenized inputs to the same length. If True, the tokenizer will
+        pad the inputs to the maximum sequence length in the batch. If False, no padding
+        will be applied.
+
+    Returns:
+    --------
+    numpy.ndarray
+        A 2D NumPy array of shape (number_of_smiles, 600), where each row corresponds to
+        the mean embedding of a SMILES string from the input list.
+
+        """
+
         chemberta = AutoModelForMaskedLM.from_pretrained('DeepChem/ChemBERTa-77M-MTR')
         tokenizer = Autotokenizer.from_pretrained('DeepChem/ChemBERTa-77M-MTR')
 
@@ -19,3 +39,4 @@ class LLMsEncoding:
                 embeddings_mean[i] = embedding
 
         return embeddings_mean.numpy()
+    
