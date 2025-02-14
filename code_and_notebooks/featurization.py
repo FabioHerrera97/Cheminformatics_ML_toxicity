d8f2446019ec64acf0214cc552dbed120721fcc0
Herrera Rocha <fabioesteban97@hotmail.com>
2025-02-14 15:40:21 +0100

    Correct test of ML encoding

--- code_and_notebooks/featurization.py
+++ code_and_notebooks/featurization.py
@@ -1,10 +1,10 @@
 import torch
 from tqdm import tqdm
-from transformers import AutoModelForMaskedLM, Autotokenizer
+from transformers import AutoModelForMaskedLM, AutoTokenizer
 
 class LLMsEncoding:
     
-    def featurize(smiles_list, model_name, padding=True):
+    def featurize(smiles_list, model_name, max_lenth, padding=True, trust_remote_code=True):
         """
     Featurizes a list of SMILES strings using the pretrained models.
 
@@ -14,22 +14,26 @@ class LLMsEncoding:
         A list of SMILES strings representing molecular structures.
     model_name : str
             The name of the pre-trained model to use for featurization.
+    max_length : int, optional (default=512)
+            The maximum length of the tokenized input. Inputs longer than this will be truncate
     padding : bool, optional (default=True)
         Whether to pad the tokenized inputs to the same length. If True, the tokenizer will
         pad the inputs to the maximum sequence length in the batch. 
+    trust_remote_code : bool, optional (default=False)
+            Whether to trust custom code in the model repository (required for MolFormer).
 
     Returns:
     --------
     numpy.ndarray
-        A 2D NumPy array of shape (number_of_smiles, 600), where each row corresponds to
+        A 2D NumPy array of shape (number_of_smiles, max_lenth), where each row corresponds to
         the embedding of a SMILES string from the input list.
 
         """
 
-        tokenizer = Autotokenizer.from_pretrained(model_name)
-        chemberta = AutoModelForMaskedLM.from_pretrained(model_name)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=trust_remote_code)
+        chemberta = AutoModelForMaskedLM.from_pretrained(model_name, trust_remote_code=trust_remote_code)
 
-        embeddings_cls = torch.zeros(len(smiles_list), 768)
+        embeddings_cls = torch.zeros(len(smiles_list), max_lenth)
 
         with torch.no_grad():
             for i, smiles in enumerate(tqdm(smiles_list)):
@@ -41,19 +45,21 @@ class LLMsEncoding:
 
         return embeddings_cls.numpy()
     
-    def main():
+def main():
     
-        smiles_list = ['CCO', 'C1=CC=CC=C1', 'CC(=O)O']
+    smiles_list = ['CCO', 'C1=CC=CC=C1', 'CC(=O)O']
 
-        print('Performing ChemBERTa embedding \n')
-        chemberta_embeddings = LLMsEncoding.featurize(smiles_list, 'DeepChem/ChemBERTa-77M-MTR')
-        print(f'{chemberta_embeddings}\n')
-        print('Performing MolFormer \n')
-        molformer_embeddings = LLMsEncoding.featurize(smiles_list, 'MolFormer/MolFormer')
-        print(f'{molformer_embeddings}\n')
-        print('Performing MolBERT \n')
-        molbert_embeddings = LLMsEncoding.featurize(smiles_list, 'MolBERT/MolBERT')
-        print(molbert_embeddings)
+    print('Performing ChemBERTa embedding \n')
+    chemberta_embeddings = LLMsEncoding.featurize(smiles_list, 'DeepChem/ChemBERTa-77M-MTR', 600)
+    print(f'{chemberta_embeddings}\n')
+    print('Performing MolFormer \n')
+    molformer_embeddings = LLMsEncoding.featurize(smiles_list, 'ibm/MolFormer-XL-both-10pct', 2362)
+    print(f'{molformer_embeddings}\n')
+    print('Performing SELFormer \n')
+    selformer_embeddings = LLMsEncoding.featurize(smiles_list, 'HUBioDataLab/SELFormer', 800)
+    print(selformer_embeddings)
 
-    if __name__ == 'main':
-        main()
+main()
+
+if __name__ == 'main':
+    main()
