b6244ffb10798a7e23bd5acc7e96767315a20fd4
Herrera Rocha <fabioesteban97@hotmail.com>
2025-02-14 14:29:24 +0100

    Added MolFormer encoding

--- code_and_notebooks/featurization.py
+++ code_and_notebooks/featurization.py
@@ -1,6 +1,6 @@
 import torch
 from tqdm import tqdm
-from transformers import AutoModelForMaskedLM, Autotokenizer
+from transformers import AutoModel, AutoModelForMaskedLM, Autotokenizer
 
 class LLMsEncoding:
 
@@ -14,29 +14,48 @@ class LLMsEncoding:
         A list of SMILES strings representing molecular structures.
     padding : bool, optional (default=True)
         Whether to pad the tokenized inputs to the same length. If True, the tokenizer will
-        pad the inputs to the maximum sequence length in the batch. If False, no padding
-        will be applied.
+        pad the inputs to the maximum sequence length in the batch. 
 
     Returns:
     --------
     numpy.ndarray
         A 2D NumPy array of shape (number_of_smiles, 600), where each row corresponds to
-        the mean embedding of a SMILES string from the input list.
+        the embedding of a SMILES string from the input list.
 
         """
 
-        chemberta = AutoModelForMaskedLM.from_pretrained('DeepChem/ChemBERTa-77M-MTR')
         tokenizer = Autotokenizer.from_pretrained('DeepChem/ChemBERTa-77M-MTR')
+        chemberta = AutoModelForMaskedLM.from_pretrained('DeepChem/ChemBERTa-77M-MTR')
 
-        embeddings_mean = torch.zeros(len(smiles_list), 600)
+        embeddings_cls = torch.zeros(len(smiles_list), 768)
 
         with torch.no_grad():
             for i, smiles in enumerate(tqdm(smiles_list)):
                 encoded_input = tokenizer(smiles, return_tensors='pt', padding=padding, truncation=True)
                 model_output = chemberta(**encoded_input)
 
-                embedding = torch.mean(model_output[0], 1)
-                embeddings_mean[i] = embedding
+                embedding = model_output[0][::, 0, ::]
+                embeddings_cls[i] = embedding
 
-        return embeddings_mean.numpy()
+        return embeddings_cls.numpy()
     
+    def featurize_MolFormer(smiles_list, padding=True):
+        
+        tokenizer = Autotokenizer.from_pretrained('MolFormer/MolFormer')
+        molformer = AutoModel.from_pretrained('MolFormer/MolFormer')
+
+        embeddings_cls = torch.zeros(len(smiles_list), 768)
+
+        with torch.no_grad():
+            for i, smiles in enumerate(tqdm(smiles_list)):
+
+                encoded_input = tokenizer(smiles_list, return_tensors='pt', padding=padding, truncation=True)
+                model_outputs = molformer (**encoded_input)
+
+                embedding = model_outputs[0][::, 0, ::]
+                embeddings_cls[i] = embedding
+
+            return embeddings_cls.numpy()
+        
+    
+
